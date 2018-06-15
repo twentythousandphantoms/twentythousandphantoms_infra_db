5697513cb73b983f6ce45183b0648f7ad74e9ef9
a.belov <a.belov@i-free.com>
2018-06-15 15:27:49 +0300

    :hatching_chick:

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+# check if MongoDB is enabled and running
+def test_mongo_running_and_enabled(host):
+    mongo = host.service("mongod")
+    assert mongo.is_running
+    assert mongo.is_enabled
+
+# check if configuration file contains the required line
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
+    assert config_file.contains('bindIp: 0.0.0.0')
+    assert config_file.is_file
+
+# check if MongoDB is lisening on port 27017
+def test_mongo_listening(host):
+    mongo_port = host.socket('tcp://0.0.0.0:27017')
+    assert mongo_port.is_listening
+
